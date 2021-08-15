e77a9c672874fe100fc47210d6d265317100ba10
Cesar Corujo <cesar.corujo@gmail.com>
2021-08-15 10:29:57 -0300

    Finished first part of conditionals

--- java.js
+++ java.js
@@ -7,19 +7,20 @@ function playerSelection (){
    let player = document.querySelector("#selection").value;
    let message = document.querySelector("#message");
    let mensaje = "Your selection is " + player ;
-  message.textContent = mensaje;
+  
 
-if (player !== "paper" || player !== "rock" || player !== "sissors"){
+if (player == "paper" || player == "Paper" || player == "PAPER" 
+|| player == "rock" || player == "Rock" || player == "ROCK"|| 
+player == "Sissors" || player == "SISSORS" || player == "sissors"){
 
-   alert("Error");
+   message.textContent = mensaje;
 }
 
 else {
-   alert("Good try");
-}
-   
+  alert("PLease type only correct options");
 }
 
+}
 
 
 // Function for cumputer random play //
@@ -27,18 +28,53 @@ else {
 
 function computerPlay() {
 
-const play = ["paper","sissors","rock"];
+const play = ["Paper","Sissors","Rock"];
 
    let randomItem = play[Math.floor(Math.random()*play.length)];
 
   document.getElementById("demo").innerHTML = "Computer Selection is " + randomItem;
 
-
+return randomItem;
 }
+
     
-function game(eventogame) {
-   eventogame.preventDefault();
-   console.log(computerPlay(),playerSelection());
+// Function to print game //
+
+function game(eventgame) {
+
+   eventgame.preventDefault();
+   computerPlay();
+   playerSelection();
+
+   let player = document.querySelector("#selection").value;
+
+   
+
+     
+   if ((computerPlay() === "Paper" && player === "paper" ) || 
+   (computerPlay() === "Sissors" && player === "sissors") || 
+   (computerPlay() === "Rock" && player === "rock" )) {
+
+      alert("Empate");
+   }
+
+   else if ((computerPlay() === "Paper" && player === "rock" )) {
+
+      alert("Paper beats rock !");
+   }
+else if ((computerPlay() === "Sissors" && player === "rock" )){
+
+   alert("Rock beat Sissors !");
+}
+
+else if((computerPlay() === "Rock" && player === "paper" )) {
+
+   alert("Paper beats Rock !");
+}
+
+   else {
+      alert("Play again");
+   }
 }
 
 let miform = document.querySelector("#form");
