0d3f665f7aa6d9e3c0d39b3a3744102c5e33468d
Cesar Corujo <cesar.corujo@gmail.com>
2021-08-21 19:44:50 -0300

    Done new total messages

--- java.js
+++ java.js
@@ -4,6 +4,7 @@
 
 function playerSelection (){
 
+
    let player = document.querySelector("#selection").value;
    let message = document.querySelector("#message");
    let mensaje = "Your selection is " + player ;
@@ -14,13 +15,16 @@ if (player === "paper" || player === "Paper" || player === "PAPER"
 player === "Sissors" || player === "SISSORS" || player === "sissors"){
 
    message.textContent = mensaje;
-}
+} 
 
 else {
+
   alert("PLease type only correct options");
+
 }
 
 }
+
 
 
 // Function for cumputer random play //
@@ -48,7 +52,9 @@ function game(eventgame) {
 
    let player = document.querySelector("#selection").value.toLowerCase();
 
-   var computer = computerPlay();
+    var computer = computerPlay();
+
+    var gamerTotal 
 
       
     
@@ -59,37 +65,62 @@ function game(eventgame) {
     {
 
       document.getElementById("alert1").innerHTML = "Draw !";
+      document.getElementById("gamerTotal").innerHTML = " Gamer =  0" ;
+      document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
+      
 
-
-} else if ((computer === "Rock" && player === "paper") || 
-(computer === "Paper" && player === "rock"))
+} else if (computer === "Rock" && player === "paper") 
 
 {
 
    document.getElementById("alert2").innerHTML = "Paper beats rock !";
+   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
 
-} else if ((computer === "Sissors" && player === "rock")
- || (computer === "Rock" && player === "sissors")) 
+} else if (computer === "Paper" && player === "rock") {
+
+   document.getElementById("alert2").innerHTML = "Paper beats rock !";
+   document.getElementById("gamerTotal").innerHTML = " Computer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Gamer  =  0" ;
+
+}
+
+ else if (computer === "Sissors" && player === "rock")
+ 
 
 {
 
    document.getElementById("alert3").innerHTML = "Rock beat Sissors !";
+   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
 
-}  
+}  else if (computer === "Rock" && player === "sissors") {
 
- else if ((computer === "Paper" && player === "sissors") || 
+   document.getElementById("alert3").innerHTML = "Rock beat Sissors !";
+   document.getElementById("gamerTotal").innerHTML = " Computer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Gamer  =  0" ;
 
-(computer === "Sissors" && player === "paper"))
+}
 
+ else if (computer === "Paper" && player === "sissors") 
 
 {
 
    document.getElementById("alert4").innerHTML = "Sissors beats Paper !";
-
+   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
  
+} else if (computer === "Sissors" && player === "paper") {
+
+   document.getElementById("alert4").innerHTML = "Sissors beats Paper !";
+   document.getElementById("gamerTotal").innerHTML = " Computer =  1" ;
+   document.getElementById("computerTotal").innerHTML = " Gamer  =  0" ;
 }
       
 }
+
+
+
  
      
 document.getElementById("form").reset()
