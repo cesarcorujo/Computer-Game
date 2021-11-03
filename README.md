5884830cf6568390b113aa3f892cfd7276a7dd53
Cesar Corujo <cesar.corujo@gmail.com>
2021-11-03 19:29:31 -0300

    updated code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Paper Sissors Rock Game
+Paper Scissors Rock Game
--- java.js
+++ java.js
@@ -1,163 +1,95 @@
 
-function playerSelection() {
+var grupoTarjetas = ["✂️", "📃", "🪨"];
 
-   let player = document.querySelector("#selection").value.toLowerCase();
+
+
+function barajaTarjetas() {
+  var resultado;
+  resultado = grupoTarjetas.sort(function () {
+    return 0.5 - Math.random();
+  });
+  return resultado;
 }
 
-let userScore = 0;
-let computerScore = 0;
 
 
-// Function for cumputer random play //
+function reparteTarjetas() {
 
+
+  var mesa = document.querySelector("#mesa");
+  var mesa1 = document.querySelector("#mesa1");
+  var mesa2 = document.querySelector("#mesa2");
+  var mesa3 = document.querySelector("#mesa3");
+
+
+  var tarjetasBarajadas = barajaTarjetas();
+
+  mesa2.innerHTML = '<span>Human Play</span>';
+  mesa3.innerHTML = '<span>Computer Play</span>';
+
+  mesa.innerHTML = "";
+  mesa1.innerHTML = "";
+
+
+
+  tarjetasBarajadas.forEach(function (elemento) {
+
+
+    var tarjeta = document.createElement("div");
+    var tarjeta1 = document.createElement("div");
+
+
+    tarjeta.innerHTML =
+      "<div  class='tarjeta1'>" +
+      "<div class='tarjeta__contenido1'>" +
+      elemento +
+      "</div>";
+
+
+    mesa1.appendChild(tarjeta1);
+
+    tarjeta1.innerHTML =
+      "<div class='tarjeta'>" +
+      "<div class='tarjeta__contenido'>" +
+      elemento +
+      "</div>" +
+      "</div>";
+
+    mesa.appendChild(tarjeta);
+
+
+  });
+
+
+}
+
+reparteTarjetas();
 
 function computerPlay() {
 
-const play = ["Paper","Sissors","Rock"];
 
-   let randomItem = play[Math.floor(Math.random()*play.length)];
+  let randomItem = tarjeta[Math.floor(Math.random() * play.length)];
 
-  document.getElementById("demo").innerHTML = "Computer selection is: " + "  " + randomItem.toUpperCase();
 
-return randomItem;
+  return randomItem;
 }
 
-//Disable computer text //
+function selection() {
 
-function disable() {
-  
-  document.getElementById("demo").innerHTML = "";
-  }
+  this.classList.add("clicking");
 
-    
-// Function to print game //
-
-
-      
-
-
-function game(eventgame) {
-
-  
-   eventgame.preventDefault();
-   computerPlay();
-
-   
-
-   let player = document.querySelector("#selection").value.toLowerCase();
-
-    var computer = computerPlay();
-
-    let message = document.querySelector("#message");
-   let mensaje = "Your selection is :  " + " " + player.toUpperCase() ;
-
-
-   
-          
-   if ((computer === "Paper" && player === "paper") || 
-   (computer === "Sissors" && player === "sissors") || 
-   (computer === "Rock" && player === "rock"))  
-   
-    {
-      
-      document.getElementById("alert1").innerHTML = "Tie !";
-      document.getElementById("gamerTotal").innerHTML = " Gamer =  0" ;
-      document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
-      message.textContent = mensaje;
-
-
-} else if (computer === "Rock" && player === "paper") 
-
-{
-   
-   message.textContent = mensaje;
-   document.getElementById("alert2").innerHTML = "Paper beats rock !";
-   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
-   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
-   userScore++;
-
-} else if (computer === "Paper" && player === "rock") {
-
-   
-   message.textContent = mensaje;
-   document.getElementById("alert2").innerHTML = "Paper beats rock !";
-   document.getElementById("gamerTotal").innerHTML = " Computer =  1" ;
-   document.getElementById("computerTotal").innerHTML = " Gamer  =  0" ;
-   computerScore++;
 }
 
- else if (computer === "Sissors" && player === "rock")
- 
 
-{
-
-  
-   message.textContent = mensaje;
-   document.getElementById("alert3").innerHTML = "Rock beat Sissors !";
-   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
-   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
-   userScore++;
-
-}  else if (computer === "Rock" && player === "sissors") {
-
- 
-   message.textContent = mensaje;
-   document.getElementById("alert3").innerHTML = "Rock beat Sissors !";
-   document.getElementById("gamerTotal").innerHTML = " Gamer =  0" ;
-   document.getElementById("computerTotal").innerHTML = " Computer  =  1" ;
-   computerScore++;
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('clicking');
 }
 
- else if (computer === "Paper" && player === "sissors") 
 
-{
-   
- 
-   message.textContent = mensaje;
-   document.getElementById("alert4").innerHTML = "Sissors beats Paper !";
-   document.getElementById("gamerTotal").innerHTML = " Gamer =  1" ;
-   document.getElementById("computerTotal").innerHTML = " Computer  =  0" ;
-   userScore++;
- 
-} else if (computer === "Sissors" && player === "paper") {
+document.querySelectorAll(".tarjeta1").forEach(function (elemento) {
+  elemento.addEventListener("click", selection);
+  elemento.addEventListener('transitionend', removeTransition);
 
-   
-   message.textContent = mensaje;
-   document.getElementById("alert4").innerHTML = "Sissors beats Paper !";
-   document.getElementById("gamerTotal").innerHTML = " Gamer =  0" ;
-   document.getElementById("computerTotal").innerHTML = " Computer  =  1" ;
-   computerScore++;
-
-} else {
-
-   disable();
-   alert("PLease type only correct options");
- 
-}
-      
-}    
-  
-
-   
-
-
- //Event to reload//
-     
-document.getElementById("form").reset()
-
-let miform = document.querySelector("#form");
-miform.addEventListener("submit",game);
-
-const reloadtButton = document.querySelector("#reload");
- //Reload everything://
-
-function reload() {
-
-    reload = location.reload();
-}
-
- //Event listeners for reload //
-reloadButton.addEventListener("click", reload, false);
-
-
+});
 
